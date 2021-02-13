2fe0db5ab6e074225fd7618255add5ddf1b36c17
Pol Van Aubel <dev@polvanaubel.com>
2021-02-13 17:50:50 +0100

    Added include dir to CMakeLists.txt

--- src/version.h
+++ src/version.h
@@ -1,7 +1,7 @@
 #ifndef VERSION_H
 #define VERSION_H
 
-#define GIT_BRANCH "master"
-#define GIT_COMMIT_HASH "adbb063"
+#define GIT_BRANCH "arch-build-fixes"
+#define GIT_COMMIT_HASH "db19e29"
 
 #endif
